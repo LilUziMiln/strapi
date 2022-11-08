0521843b7fdca529dd8452de2b61b755ec46822f
michiel.verbeke <michiel.verbeke@student.howest.be>
2022-11-08 11:50:42 +0100

    update

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,12 @@
 module.exports = ({ env }) => ({
     "vercel-deploy": {
         enabled: true,
-    }
+        config: {
+            deployHook: "https://api.vercel.com/v1/integrations/deploy/prj_MgnjMG60IHocp2IMEKMEBZ0GYEk8/BzYrBdVyin",
+            apiToken: "qzVlncATnun3DkEpcM6pwbHR",
+            appFilter: "strapi",
+            teamFilter: "liluzimiln",
+            roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+        },
+    },
 });
