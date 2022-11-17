a24f1285b974a7d8a59c611a80f23194215bd015
michiel.verbeke <michiel.verbeke@student.howest.be>
2022-11-17 17:21:19 +0100

    added tracing

--- config/plugins.js
+++ config/plugins.js
@@ -18,7 +18,7 @@ module.exports = ({ env }) => ({
             depthLimit: 7,
             amountLimit: 100,
             apolloServer: {
-                tracing: false,
+                tracing: true,
             },
         },
     },
